e62ed9777fb46bdad9aa0b42e0008e7a15176f88
alecz86@gmail.com <GITHUBEMAIL>
2025-03-22 17:36:47 +0000

    added module

---
+++ appmodule/main.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "application" {
+  name      = var.name
+  namespace = var.namespace
+  chart     = "${path.module}/application"
+  wait      = false
+  values = [
+    var.values_yaml
+  ]
+}
+
+
+
